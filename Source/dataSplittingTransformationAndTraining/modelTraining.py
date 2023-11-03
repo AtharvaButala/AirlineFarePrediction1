b85b0cba6a0e63251c6b990feb2413cc4f1d3df7
ATHARVA <atharvabutala23@gmail.com>
2023-11-03 11:49:05 +0530

    Final Project

---
+++ Source/dataSplittingTransformationAndTraining/modelTraining.py
@@ -0,0 +1,116 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.svm import SVR
+
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from ExceptionLoggerAndUtils.logger import App_Logger
+from ExceptionLoggerAndUtils.exception import CustomException
+#from xgboost import XGBRegressor
+
+from ExceptionLoggerAndUtils.utils import save_object, evaluate_Regression_models
+
+
+class ModelTrainerClass:
+    def __init__(self):
+        self.log_writer = App_Logger()
+        self.trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+    def modelsToTrainAndParameters(self):
+        try:
+            models = {
+                "Random Forest"         : RandomForestRegressor(),
+                "Gradient Boosting"     : GradientBoostingRegressor(),
+                "Linear Regression"     : LinearRegression(),
+                "lasso"                 : Lasso(),
+                "ridge"                 : Ridge(),
+                "AdaBoost Regressor"    : AdaBoostRegressor(),
+                "SVR"                   : SVR()
+
+            }
+
+            params = {
+                    "Random Forest": {
+                        'n_estimators': [10, 50, 100, 200],  # Number of trees in the forest
+                        'max_depth': [None, 10, 20, 30],   # Maximum depth of each tree
+                        'min_samples_split': [2, 5, 10],  # Minimum samples required to split a node
+                        'min_samples_leaf': [1, 2, 4],    # Minimum samples required in a leaf node
+                        'max_features': ['auto', 'sqrt', 'log2'],  # Number of features to consider
+                    },
+                        "Gradient Boosting": {
+                        'n_estimators': [100, 200, 300],  # Number of boosting stages (trees)
+                        'learning_rate': [0.01, 0.1, 0.2],  # Step size shrinkage to prevent overfitting
+                        'max_depth': [3, 4, 5],  # Maximum depth of each tree
+                        'min_samples_split': [2, 5, 10],  # Minimum samples required to split a node
+                        'min_samples_leaf': [1, 2, 4],  # Minimum samples required in a leaf node
+                        'max_features': ['auto', 'sqrt', 'log2'],  # Number of features to consider
+                    },
+                    "Linear Regression": {
+                        #'alpha': [0.01, 0.1, 1.0, 10.0],  # Regularization strength (alpha)
+                        'fit_intercept': [True, False]  # Whether to fit the intercept
+                    },
+                    "lasso": {
+                        'alpha': [0.01, 0.1, 1.0, 10.0],  # Regularization strength (alpha)
+                        'fit_intercept': [True, False]  # Whether to fit the intercept
+                    },
+                    "ridge": {
+                        'alpha': [0.01, 0.1, 1.0, 10.0],  # Regularization strength (alpha)
+                        'fit_intercept': [True, False]  # Whether to fit the intercept
+                    },
+                    "AdaBoost Regressor": {
+                        'n_estimators': [50, 100, 200],
+                        'learning_rate': [0.1, 0.01, 0.001],
+                        'base_estimator': [DecisionTreeRegressor(max_depth=3), DecisionTreeRegressor(max_depth=5)],
+                        'loss': ['linear', 'square', 'exponential']
+                    },
+                    "SVR": {
+                        'C': [0.1, 1, 10],
+                        'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+                    },
+                }
+
+            return models , params
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+    def modelTraingMethod(self,X_train, X_test, y_train, y_test,models,params):
+        try:
+
+            model_report: dict = evaluate_Regression_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
+                                                 models=models, param=params)
+
+            ## below returns the best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+
+            ## below returns the best model Name score from dict
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+
+            ## To get best model name from dict
+            best_model = models[best_model_name]
+
+            print("Best found model on both training and testing dataset")
+            print(best_model)
+
+            save_object(
+                file_path=self.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e, sys)
---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, render_template
+from flask_cors import cross_origin
+import sklearn
+import pickle
+import pandas as pd
+from Source.prediction.predictionPipline import CustomData
+from Source.preproMethods.dataReadingAndCleaning import dataReadingAndCleaningClass
+from Source.prediction.predictionPipline import PredictPipline
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@cross_origin()
+
+def home():
+    return render_template("home.html")
+
+@app.route("/predict", methods = ["GET", "POST"])
+@cross_origin()
+
+def predict():
+    if request.method == "GET":
+        return render_template(home.html)
+    else:
+
+        data = CustomData(Airline = request.form.get('Airline'),
+                          Date_of_Journey = request.form.get('Dep_Time'),
+                          Source=request.form.get('Source'),
+                          Destination=request.form.get('Destination'),
+                          Dep_Time=request.form.get('Dep_Time'),
+                          Arrival_Time=request.form.get('Arrival_Time'),
+                          Duration=request.form.get('Duration'),
+                          Total_Stops=request.form.get('Total_Stops'))
+
+        pred_df = data.getDataAsDataFrame()
+        pred_df = data.changeDatatypeOfColumn(pred_df)
+        pred_df = data.convertDateInToDayMonthYear(pred_df)
+
+        dataReadingAndCleaningC = dataReadingAndCleaningClass()
+
+        pred_df = dataReadingAndCleaningC.convertHoursAndMinutesToIndependantColumns(df=pred_df, columName="Dep_Time")
+        pred_df = dataReadingAndCleaningC.convertHoursAndMinutesToIndependantColumns(df=pred_df,
+                                                                                     columName="Arrival_Time")
+        pred_df = data.isertValueInDuration(pred_df)
+        pred_df = dataReadingAndCleaningC.convertDurationToMinutes(pred_df)
+        pred_df = data.dropUncessaryColumns(pred_df)
+        PredictPipeline1 = PredictPipline()
+        output = PredictPipeline1.predict(pred_df)
+
+
+        return render_template('home.html', prediction_text="Your Flight price is Rs. {}".format(output))
+
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
